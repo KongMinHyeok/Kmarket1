9f067a2d71e257fa09fba9fb36c6e7266a099bda
SleepingAlbaricoque <tnqls0421@naver.com>
2022-12-08 10:21:53 +0900

    2022.12.08 멤버 테이블 쿼리문

---
+++ 멤버 테이블 쿼리문.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE Kmarket;
+
+CREATE TABLE km_member(
+uid VARCHAR(20) NOT NULL PRIMARY KEY, 
+pass VARCHAR(255) NOT NULL,
+name VARCHAR(20) NOT NULL,
+gender TINYINT NOT NULL,
+hp CHAR(13) NOT NULL,
+email VARCHAR(100) NOT NULL,
+type TINYINT NOT NULL,
+point INT DEFAULT '0',
+level TINYINT DEFAULT '1',
+zip VARCHAR(10),
+addr1 VARCHAR(255),
+addr2 VARCHAR(255),
+company VARCHAR(20),
+ceo VARCHAR(20),
+bizRegNum CHAR(12),
+comRegNum VARCHAR(100),
+tel VARCHAR(20),
+manager VARCHAR(20),
+managerHp CHAR(13),
+fax VARCHAR(20),
+regip VARCHAR(100) NOT NULL,
+wdate DATETIME,
+rdate DATETIME NOT NULL,
+etc1 INT,
+etc2 INT,
+etc3 VARCHAR(10),
+etc4 VARCHAR(20),
+etc5 VARCHAR(30)
+);
+
+CREATE TABLE km_member_point(
+pointNo INT AUTO_INCREMENT PRIMARY KEY,
+uid VARCHAR(20) NOT NULL,
+ordNo INT NOT NULL,
+point INT NOT NULL,
+pointDate DATETIME NOT null
+);
+
+CREATE TABLE km_member_terms(
+terms TEXT NOT NULL,
+privacy TEXT NOT NULL,
+location TEXT NOT NULL,
+finance TEXT NOT NULL,
+tax TEXT NOT null
+);
