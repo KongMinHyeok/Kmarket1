c13bb3dd4a13005a62f9b73468cc99583fe5f199
JavApext <JavApext@gmail.com>
2022-12-14 13:52:11 +0900

    admin 작업중, 카테고리 쿼리문 추가

--- Kmarket1/src/main/java/kr/co/kmarket1/db/AdminSQL.java
+++ Kmarket1/src/main/java/kr/co/kmarket1/db/AdminSQL.java
@@ -1,30 +1,9 @@
 package kr.co.kmarket1.db;
 
 public class AdminSQL {
-
-	public static final String SELECT_PRODUCTS = "SELECT * FROM `km_product` "
-												+ "WHERE `prodCate2` =? "
-												+ "ORDER BY `prodNo` DESC limit ?, 10";
-
-	public static final String SELECT_COUNT_TOTAL = "SELECT COUNT(`prodNo`) FROM `km_product` "
-												+ "where `prodCate`=?";
-	
-	public static final String INSERT_PRODUCT = "INSERT INTO `km_product` set "
-												+ "`prodName`=? "
-												+ "`descript`=? "
-												+ "`company`=? "
-												+ "`price`=? "
-												+ "`discount`=? "
-												+ "`point`=? "
-												+ "`stock`=? "
-												+ "`delivery`=? "
-												+ "`thumb1`=? "
-												+ "`thumb2`=? "
-												+ "`thumb3`=? "
-												+ "`detail`=? "
-												+ "`status`=? "
-												+ "`duty`=? "
-												+ "`receipt`=? "
-												+ "`bizType`=? "
-												+ "`origin`=?";
+	//product_register
+	public static final String INSERT_PRODUCT_REGISTER = "INSERT INTO `km_product` SET "
+													   + "`prodName`=?, `descript`=?, `company`=?, `price`=?, `discount`=?, "
+													   + "`point`=?, `stock`=?, `delivery`=?, `thumb1`=?, `thumb2`=?, `thumb3`=?, "
+													   + "`detail`=?, `status`=?, `duty`=?, `receipt`=?, `bizType`=?, `origin`=?";
 }
